9a124c2068bb6ecca5054ab0524d5a0b28d8e111
lee1136 <leetouch1@naver.com>
2024-10-06 14:09:02 +0900

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -2,21 +2,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 // Firebase 설정
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+  apiKey: "AIzaSyBy00Auvz1psRo7YzfgsXrAbe50zitO5IA",
+  authDomain: "wlrn111.firebaseapp.com",
+  projectId: "wlrn111",
+  storageBucket: "wlrn111.appspot.com",
+  messagingSenderId: "542337866692",
+  appId: "1:542337866692:web:4f9e24b346e438cccc2291",
+  measurementId: "G-R29WJLP1FG"
 };
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 // Firebase 객체를 내보내기
-export { auth, db };
+export { auth, db, storage };
